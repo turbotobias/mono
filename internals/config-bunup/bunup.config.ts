58e3d8f0278f81d89e8a1e9cb0b77d9656686697
Tobias Goulden Schultz <tob@hey.com>
2025-06-04 06:29:09 +0200

    setup: vscode settings

--- internals/config-bunup/bunup.config.ts
+++ internals/config-bunup/bunup.config.ts
@@ -1,12 +1,22 @@
-import type { DefineWorkspaceItem } from "bunup"
+import { get_dirs_from_path } from "@/script-bun"
+import { defineWorkspace } from "bunup"
+import { get_bunup_config } from "./bunup.config.utils"
 
-export const get_bunup_config = ({ name, root, entry = "src/index.ts" }: { name: string, root: string, entry: string }): DefineWorkspaceItem => ({
-  name,
-  root,
-  config: {
-    entry,
-    format: ["esm"],
-    dts: true,
-    sourcemap: "linked", // +4% increase in size
-  }
-})
+const [dirs_sources, dirs_outputs] = await Promise.all([
+  get_dirs_from_path("./sources"),
+  get_dirs_from_path("./outputs")
+])
+
+const ws_sources = dirs_sources.map((dir) => get_bunup_config({
+  name: dir,
+  root: `sources/${dir}`,
+  entry: "src/index.ts",
+}))
+
+const ws_outputs = dirs_outputs.map((dir) => get_bunup_config({
+  name: dir,
+  root: `outputs/${dir}`,
+  entry: "src/index.ts",
+}))
+
+export default defineWorkspace([...ws_sources, /** ...ws_outputs */])
